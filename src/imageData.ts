49b61379a9d277ea299d88e0ed0d057e67b876f7
alexchidiac <alexchidiac1337@gmail.com>
2024-12-12 14:43:19 +0100

    Random images seed

---
+++ src/imageData.ts
@@ -0,0 +1,46 @@
+export const images = [
+    'https://upload.wikimedia.org/wikipedia/commons/e/ea/Le_restaurant.JPG',
+    'https://upload.wikimedia.org/wikipedia/commons/2/23/Restaurant_1_Place_Vend%C3%B4me.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/commons/2/2b/Rodinov%2C_restaurant.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/d/d0/Restaurant_Le_Parc.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/commons/2/2b/Rodinov%2C_restaurant.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/8/8b/Restaurant_So_Lounge.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/commons/9/99/Restaurant_la_Serre.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/4/4c/Restaurant_catalan.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/commons/7/7f/Restaurant_L%27Office_%28Paris_9%C3%A8me%29.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/69/D%C4%9Bkov%2C_restaurant.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/commons/8/81/Restaurant_le_B..jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/3/3e/Le_restaurant_des_Trois_Baudets.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/fr/c/cf/Louvre-Lens_-_Restaurant.JPG',
+    'https://upload.wikimedia.org/wikipedia/commons/6/66/L%27abreuvoir_restaurant.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/commons/3/3e/Restaurant_Marc_Veyrat_001.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/e/e9/Old_Town_Restaurant.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/commons/0/06/Restaurant_de_la_M%C3%89CA.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/3/35/Restaurant_%22Chez_Bobonne%22.jpg',
+
+
+
+
+    'https://upload.wikimedia.org/wikipedia/commons/5/5c/Restaurant_La_Maison_d%27H%C3%A9l%C3%A8ne.JPG',
+    'https://upload.wikimedia.org/wikipedia/commons/6/6d/Restaurant_La_Couronne_-_panoramio.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/commons/0/09/Pont_d%27Espagne_restaurant.JPG',
+    'https://upload.wikimedia.org/wikipedia/commons/8/8b/Restaurant_lasserre_P1000432.JPG',
+
+    'https://upload.wikimedia.org/wikipedia/commons/1/1d/Restaurant_in_The_Mus%C3%A9e_d%27Orsay.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/8/83/Hotel-restaurant_hassendorfer.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/commons/7/7e/Restaurant_Del_Arte_au_Mans.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/3/38/Siradan_-_H%C3%B4tel_Restaurant.jpg',
+
+    'https://upload.wikimedia.org/wikipedia/commons/1/13/White_Tower_Restaurant.jpg',
+  ];
+  
